836ad976a1cf4add4178c49aa15eedb63a0ac563
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-01-07 22:42:30 +0000

    add error handling

--- assets/js/features/mobileMenu.js
+++ assets/js/features/mobileMenu.js
@@ -1,10 +1,21 @@
 import Menu from "../classes/Menu.js";
 
-const mobileMenu = new Menu(
-  document.querySelector("#mobile-menu-button"),
-  document.querySelector("#mobile-menu-content"),
-  document.querySelector("#mobile-menu-first-focus"),
-  document.querySelector("#mobile-menu-last-focus")
-);
+try {
+  const elements = [
+    "#mobile-menu-button",
+    "#mobile-menu-content",
+    "#mobile-menu-first-focus",
+    "#mobile-menu-last-focus",
+  ].map((selector) => {
+    const el = document.querySelector(selector);
+    if (!el) {
+      throw new Error(`Element not present in the DOM: ${selector}`);
+    }
+    return el;
+  });
 
-mobileMenu.init();
+  const mobileMenu = new Menu(...elements);
+  mobileMenu.init();
+} catch (e) {
+  console.error(e);
+}
