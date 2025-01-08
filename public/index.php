c882f1cd94fd98c90f795db82e65be690a8eaf2f
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-01-08 02:40:39 +0000

    file: move/rename, refactor directory structure

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
+
+$title = "Jiggy's Web Fundamentals";
+$content_file = __DIR__ . "/../templates/content/home.php";
+
+require_once __DIR__ . "/../templates/layout.php";
---
+++ templates/layout.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./assets/css/global.css">
+  <title><?php echo $title; ?></title>
+</head>
+
+<body>
+  <div id="container">
+    <?php require_once __DIR__ . "/components/header.php"; ?>
+    <div id="content">
+      <?php require_once $content_file; ?>
+    </div>
+  </div>
+</body>
+
+</html>
