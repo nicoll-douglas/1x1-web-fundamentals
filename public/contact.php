bb52d79742c1bd35f97f93aaa4964e619fd82384
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-01-08 20:50:59 +0000

    add contact page

---
+++ public/contact.php
@@ -0,0 +1,8 @@
+<?php
+
+$title = "Contact | Jiggy's Web Fundamentals";
+$content_file = __DIR__ . "/../templates/content/contact.php";
+$footer_contact = false;
+$css_hrefs = ["/assets/css/contact.css"];
+
+require_once __DIR__ . "/../templates/layout.php";
---
+++ templates/content/contact.php
@@ -0,0 +1,21 @@
+<main>
+  <h1>Contact</h1>
+  <section>
+    <h2>Email</h2>
+    <p>
+      Feel free to shoot me an email at the address down below, especially if you need help with anything I&apos;ve talked about in a tutorial or have any feedback.</p>
+    <a href="mailto:dev.nicoll.douglas@gmail.com">dev.nicoll.douglas@gmail.com</a>
+  </section>
+  <section>
+    <h2 id="leave-a-message-heading">Leave a Message</h2>
+    <p>If you wish to leave an anonymous message!</p>
+    <?php require_once __DIR__ . "/../components/message_form.php"; ?>
+  </section>
+  <section>
+    <h2>Contributing</h2>
+    <p>If you wish to contribute to this project I&apos;ll leave a link to the GitHub repo down below.</p>
+    <a href="https://github.com/nicoll-douglas/jiggys-web-fundamentals" target="_blank">
+      https://github.com/nicoll-douglas/jiggys-web-fundamentals
+    </a>
+  </section>
+</main>
