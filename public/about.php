28416493a500f11344b57c62dcb6f79e479391a7
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-01-08 18:46:49 +0000

    style: edit, content: add about page

---
+++ public/about.php
@@ -0,0 +1,6 @@
+<?php
+
+$title = "About | Jiggy's Web Fundamentals";
+$content_file = __DIR__ . "/../templates/content/about.php";
+
+require_once __DIR__ . "/../templates/layout.php";
---
+++ templates/content/about.php
@@ -0,0 +1,34 @@
+<main>
+  <article>
+    <h1>About Jiggy&apos;s Web Fundamentals</h1>
+    <section>
+      <h2 id="background">Background</h2>
+      <p>Jiggy&apos;s Web Fundamentals is a project created by me, <a href="https://nicolldouglas.dev/" target="_blank">Nicoll Douglas</a>, or as people on the internet know me: Jiggy. I&apos;m a self-taught programmer who has been learning web development since late 2023.</p>
+      <p>
+        So far in my journey I&apos;ve watched countless tutorials, spent countless days reading documentation, building projects, and all the other 101 things you go through on the self-taught programming path. I&apos;ve made it quite far, and Jiggy&apos;s Web Fundamentals is my attempt at compiling the most essential and practical knowledge I&apos;ve learnt, as well as the resource I would&apos;ve wished to have as I was learning.
+      </p>
+      <p>The tutorials I provide help you learn the fundamentals that are necessary and beneficial for building on the web. These include frontend technologies such as HTML, CSS and JavaScript; backend technologies such as PHP and MySQL; and more general things suchs as Git, Web Security, and how the internet works. The last item is perhaps one of the more important. If you have a grasp of what the hell the web is and does, you will have a better grasp of what it is <strong>you</strong> as a web developer are trying to do.</p>
+    </section>
+    <section>
+      <h2>The State of the Web</h2>
+      <p>Any other web developer out there knows that the web dev practice and ecosystem has become overwhelmingly complex over the last decade. The kinds of things that we can and want to do on the web have demanded more complexity. As a result, the ecosystem and community is flooded with a million and one technologies, abstractions, attitudes, content creators, and overall just general noise.</p>
+      <p>
+        What this means is that if you are someone trying to get into web dev, you will find a lot of conflicting and opinionated views about how to get into web dev, or whether you should get into it at all. The aim here is not to dispute those views with my own take, but rather provide some basic sense amongst the noise for those that are interested in web dev.
+      </p>
+      <p>
+        A strong understanding of these fundamentals and building with basic web tools means you will have a core understanding of how any complex website or web app works under the hood and be able to build whatever you wish. One of the goals of Jiggy&apos;s Web Fundamentals is to turn the complex into the simple. And if you have an understanding of the simple, you have an understanding of the complex.
+      </p>
+      <p>
+        You will also understand why the web dev ecosystem has evolved the way it has, the changing attitudes, and be able to form your own opinions that are often fed to you by people you think are more qualified. All of this just by learning the fundamentals, powerful stuff right?
+      </p>
+    </section>
+    <section>
+      <h2>Final Note</h2>
+      <p>I believe that one of the joys of web dev and programming as a whole, is that you have an incredible amount of creative freedom to build whatever you can think up. If you&apos;re someone pragmatic, solve that problem you wish you had a solution for. If you&apos;re someone more artsy, the web is your perfect canvas.</p>
+      <p>
+        Back in the old days of the web, this potential was one of the web&apos;s wonders that has been lost today and replaced with homogenous, uninspiring, web services and products.
+      </p>
+      <p>One of the best skills and qualities you can have is a curiosity and motivation to learn and build. Web dev and programming in general facilitates that and its a quality that will always be needed. So if you are interested in learning web dev, get inspired, take a look around, and happy learning!</p>
+    </section>
+  </article>
+</main>
