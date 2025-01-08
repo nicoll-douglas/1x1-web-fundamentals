9a1c899f2c50bb2790d3b50a9d72937777fdab51
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-01-08 03:55:08 +0000

    update styles and home page content

--- templates/content/home.php
+++ templates/content/home.php
@@ -1,2 +1,20 @@
-<h1>Hello world</h1>
-<p>Welcome to Jiggy&apos;s Web Fundamentals.</p>
+<main>
+  <h1>Hello world.</h1>
+  <h2>(Yapping)</h2>
+  <p>Welcome to Jiggy&apos;s Web Fundamentals; the no-nonsense one-stop shop for learning the fundamentals of how to build websites and web applications. </p>
+  <p>
+    Backed by an independent, self-taught developer, Jiggy's Web Fundamentals is a series of tutorials where you learn to build on the web from the ground up using HTML, CSS, JavaScript, PHP and MySQL. If those terms don&apos;t mean anything to you now but you are curious, why not check it out! It&apos;s free!
+  </p>
+  <p>
+    Here the goal is take the large and complicated scope of web development and transform it into the simple thing it really is under the surface. If you&apos;re looking for a solid foundation for a career in web development, are just a hobbyist, or want the insights of your average web developer, this is the place for you!
+  </p>
+  <h2>Overview</h2>
+  <p>These tutorials are organised with a 3-level hierarchy. Modules > Chapters > Tutorials: Modules cover a broad topic (e.g <a href="/course/html/index.php">HTML</a>), chapters cover core subtopics (e.g <a href="/course/html/forms/index.php">Forms</a>) and tutorials cover key aspects of each subtopic (e.g <a href="/course/html/the-input-element.php">The &lt;input /&gt; element</a>).</p>
+  <p>Get the full picture: <a href="/course/index.php">Course Index</a>.</p>
+  <p>
+    Get started:
+    <a href="/course/introduction/how-the-web-works/clients-and-servers.php">
+      How the Web Works: Clients and Servers
+    </a>
+  </p>
+</main>
