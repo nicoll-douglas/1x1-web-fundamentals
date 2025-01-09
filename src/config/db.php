88b43349401336cf2e005af9acd799dae83546d2
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-01-09 16:37:26 +0000

    file: add database configurations

---
+++ src/config/db.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once __DIR__ . "/env.php";
+
+function connectDB()
+{
+  $db_host = getenv("DB_HOST");
+  $db_name = getenv("DB_NAME");
+  $db_user = getenv("DB_USER");
+  $db_pass = getenv("DB_PASS");
+
+  try {
+    $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $pdo;
+  } catch (PDOException $e) {
+    return false;
+  }
+}
