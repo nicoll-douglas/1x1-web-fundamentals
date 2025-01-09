98a55838925e2da02b9720050fb887d85f99d331
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-01-09 14:15:24 +0000

    file: add .gitignore and .env loading script

---
+++ src/utils/env.php
@@ -0,0 +1,21 @@
+<?php
+
+$env_file = __DIR__ . "/../.env";
+
+if (file_exists($env_file)) {
+  $lines = file($env_file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+  foreach ($lines as $line) {
+    if (strpos(trim($line), "#") === 0) {
+      continue;
+    }
+
+    [$key, $value] = explode("=", $line, 2);
+    $key = trim($key);
+    $value = trim($value);
+
+    putenv("$key=$value");
+  }
+} else {
+  throw new RuntimeException("Missing .env");
+}
