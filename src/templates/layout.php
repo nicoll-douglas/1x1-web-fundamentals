ca57572bfb3dcde53dbdd352abebbb5d1193f31a
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-01-09 16:37:11 +0000

    refactor: css

--- src/templates/layout.php
+++ src/templates/layout.php
@@ -5,7 +5,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="/assets/css/global.css">
+  <link rel="stylesheet" href="/assets/css/layout.css">
   <title><?php echo $title; ?></title>
+  <?php require_once __DIR__ . "/components/font.php"; ?>
   <?php
   if (isset($css_hrefs)):
     foreach ($css_hrefs as $css_href):
