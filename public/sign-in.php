66f93d52f3718606bd90eae82b541ddd935993a3
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-01-10 01:01:19 +0000

    add redirection logic for sign-in page if auth'd

--- public/sign-in.php
+++ public/sign-in.php
@@ -1,5 +1,13 @@
 <?php
 
+
+require_once __DIR__ . "/../src/middleware/authorization.php";
+
+if (checkAuthorization()) {
+  header("Location: /tutorials/index.php");
+  exit;
+}
+
 $title = "Sign In | Jiggy's Web Fundamentals";
 $view = __DIR__ . "/../src/views/sign-in.php";
 $css_hrefs = ["/assets/css/sign-in.css"];
