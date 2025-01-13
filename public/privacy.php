f0a1cc649f15357e56ee07ec9a7fd6915ea68d81
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-01-13 14:31:58 +0000

    content: add privacy page

---
+++ public/privacy.php
@@ -0,0 +1,7 @@
+<?php
+
+$title = "Privacy | Jiggy's Web Fundamentals";
+
+$view =  __DIR__ . "/../src/views/privacy.php";
+
+require_once __DIR__ . "/../src/templates/layout.php";
---
+++ src/views/privacy.php
@@ -0,0 +1,26 @@
+<main>
+  <h1>Privacy</h1>
+  <p>I do not sell your data or collect analytics. Below is a quick rundown of how your data is used for the site to function:</p>
+  <section>
+    <h2>What&apos;s Collected</h2>
+    <p>
+      When you sign in using OAuth, your name and account ID are used to help you maintain your account.
+    </p>
+    <p>Your IP address is also hashed and stored to maintain the security of your session.</p>
+  </section>
+  <section>
+    <h2>More Specifically</h2>
+    <p>Your data is used to help you:</p>
+    <ul>
+      <li>Log in to the site.</li>
+      <li>Stay logged into the site.</li>
+      <li>Personalise your experience (showing your name).</li>
+      <li>Save your progress on the tutorials.</li>
+    </ul>
+  </section>
+  <section>
+    <h2>Your Control</h2>
+    <p>You can revoke access to your information any time by managing app permissions in your Google account settings and clicking the link below to purge your data and your session.</p>
+    <a href="/api/auth/delete.php">Delete my data</a>
+  </section>
+</main>
