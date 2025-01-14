d860b84a82ddfda03cc66c774d756ee2f46fa088
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-01-14 20:13:51 +0000

    delete files

--- public/tutorials/the-web/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$title = 'The Web | Jiggy\'s Web Fundamentals';
-$view = __DIR__ . "/../../../src/views/tutorials/the-web/index.php";
-
-require_once __DIR__ . "/../../../src/templates/layout.php";
