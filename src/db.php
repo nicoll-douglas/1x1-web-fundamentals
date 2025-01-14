b9aebec8ee4d936baa980c51225387a188ae3cf6
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-01-14 18:56:28 +0000

    refactor: database connection function to not catch errors

--- src/db.php
+++ src/db.php
@@ -4,20 +4,17 @@ require_once __DIR__ . "/config/env.php";
 
 /**
  * Establishes a connection to the database.
- * @return PDO|false A PDO instance if successfully connected, false otherwise.
+ * @return PDO A PDO instance if successfully connected, false otherwise.
+ * @throws PDOException If the function failed to connect to the database.
  */
-function connectDB(): PDO|false
+function connectDB(): PDO
 {
   $db_host = getenv("DB_HOST");
   $db_name = getenv("DB_NAME");
   $db_user = getenv("DB_USER");
   $db_pass = getenv("DB_PASS");
 
-  try {
-    $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    return $pdo;
-  } catch (PDOException $e) {
-    return false;
-  }
+  $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  return $pdo;
 }
