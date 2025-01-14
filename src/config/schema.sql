b1ceef945f315dbe8f2da93dd1b40d6cdc7d75ff
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-01-14 18:55:35 +0000

    update database schema

--- src/config/schema.sql
+++ src/config/schema.sql
@@ -15,3 +15,25 @@ CREATE TABLE users (
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+
+CREATE TABLE tutorial_modules (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  number INT NOT NULL,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE tutorials (
+  href VARCHAR(255) PRIMARY KEY,
+  number INT NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  module_id INT,
+  FOREIGN KEY(module_id) REFERENCES tutorial_modules(id) ON DELETE CASCADE
+);
+
+CREATE TABLE tutorial_completions (
+  tutorial_href VARCHAR(255),
+  user_id VARCHAR(255),
+  PRIMARY KEY(tutorial_href, user_id),
+  FOREIGN KEY(tutorial_href) REFERENCES tutorials(href) ON DELETE CASCADE,
+  FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+);
