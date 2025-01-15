45a6fc43f7a85d16b12fe1fc35950414ba369796
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-01-15 12:52:49 +0000

    add tutorial / tutorial completion rendering

--- src/views/tutorials/index.php
+++ src/views/tutorials/index.php
@@ -1,11 +1,48 @@
 <h1>Tutorial Index</h1>
-<section>
-  <a href="./the-web/index.php">
-    <h2>The Web</h2>
-    <ol class="indent-list">
+<?php
+foreach (
+  $tutorials as [
+    "tutorial_name" => $tname,
+    "tutorial_href" => $thref,
+    "tutorial_number" => $tnum,
+    "module_name" => $mname,
+    "is_completed" => $compl
+  ]
+):
+?>
+  <?php if ($tnum === 1): ?>
+    <section>
+      <h2><?php echo $mname; ?></h2>
+      <ol class="indent-list tutorial-list">
+      <?php endif; ?>
+
       <li>
-        <a href="./the-web/how-the-web-works.php">How the Web Works</a>
+        <?php if (isset($compl)): ?>
+          <div class="tutorial-with-check">
+
+            <?php if ($compl === 1): ?>
+              <input type="checkbox" id="<?php echo $thref; ?>" checked />
+            <?php else: ?>
+              <input type="checkbox" id="<?php echo $thref ?>" />
+            <?php endif; ?>
+
+            <label for="<?php echo $thref; ?>">
+              <a href="<?php echo $thref; ?>">
+                <?php echo $tname; ?>
+              </a>
+            </label>
+
+          </div>
+        <?php else: ?>
+
+          <a href="<?php echo $thref; ?>"><?php echo $tname; ?></a>
+
+        <?php endif; ?>
       </li>
-    </ol>
-  </a>
-</section>
+
+      <?php if ($tnum === 1): ?>
+      </ol>
+    </section>
+  <?php endif; ?>
+
+<?php endforeach; ?>
