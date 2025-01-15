e61d92acbb544617bd610db5789286d8290d3885
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-01-15 13:16:56 +0000

    fix: li counter increment

--- src/views/tutorials/index.php
+++ src/views/tutorials/index.php
@@ -1,48 +1,54 @@
-<h1>Tutorial Index</h1>
-<?php
-foreach (
-  $tutorials as [
-    "tutorial_name" => $tname,
-    "tutorial_href" => $thref,
-    "tutorial_number" => $tnum,
-    "module_name" => $mname,
-    "is_completed" => $compl
-  ]
-):
-?>
-  <?php if ($tnum === 1): ?>
-    <section>
-      <h2><?php echo $mname; ?></h2>
-      <ol class="indent-list tutorial-list">
-      <?php endif; ?>
-
-      <li>
-        <?php if (isset($compl)): ?>
-          <div class="tutorial-with-check">
-
-            <?php if ($compl === 1): ?>
-              <input type="checkbox" id="<?php echo $thref; ?>" checked />
-            <?php else: ?>
-              <input type="checkbox" id="<?php echo $thref ?>" />
-            <?php endif; ?>
-
-            <label for="<?php echo $thref; ?>">
-              <a href="<?php echo $thref; ?>">
-                <?php echo $tname; ?>
-              </a>
-            </label>
-
-          </div>
-        <?php else: ?>
-
-          <a href="<?php echo $thref; ?>"><?php echo $tname; ?></a>
-
+<main>
+  <h1>Tutorial Index</h1>
+  <?php
+  for ($i = 0; $i < count($tutorials); $i++):
+    [
+      "tutorial_name" => $tname,
+      "tutorial_href" => $thref,
+      "tutorial_number" => $tnum,
+      "module_name" => $mname,
+      "module_number" => $mnum,
+      "is_completed" => $compl
+    ] = $tutorials[$i];
+  ?>
+    <?php if ($tnum === 1): ?>
+      <section>
+        <h2><?php echo $mname; ?></h2>
+        <ol class="indent-list tutorial-list">
         <?php endif; ?>
-      </li>
 
-      <?php if ($tnum === 1): ?>
-      </ol>
-    </section>
-  <?php endif; ?>
+        <li>
+          <?php if (isset($compl)): ?>
+            <div class="tutorial-with-check">
 
-<?php endforeach; ?>
+              <?php if ($compl === 1): ?>
+                <input type="checkbox" id="<?php echo $thref; ?>" checked />
+              <?php else: ?>
+                <input type="checkbox" id="<?php echo $thref; ?>" />
+              <?php endif; ?>
+
+              <label for="<?php echo $thref; ?>">
+                <a href="<?php echo $thref; ?>">
+                  <?php echo $tname; ?>
+                </a>
+              </label>
+
+            </div>
+          <?php else: ?>
+
+            <a href="<?php echo $thref; ?>"><?php echo $tname; ?></a>
+
+          <?php endif; ?>
+        </li>
+
+        <?php
+        $next_tut = $tutorials[$i + 1];
+        if (!isset($next_tut) || $next_tut["module_number"] > $mnum):
+        ?>
+        </ol>
+      </section>
+    <?php endif; ?>
+
+  <?php endfor; ?>
+  <button>Save</button>
+</main>
