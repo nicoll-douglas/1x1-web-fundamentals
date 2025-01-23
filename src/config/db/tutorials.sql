8948f0e971badf7ea42f658c6180a99369aa41aa
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-01-23 17:42:56 +0000

    file: rename / update db schema

--- src/config/db/tutorials.sql
+++ src/config/db/tutorials.sql
@@ -6,3 +6,6 @@ VALUES ("/tutorials/the-web/how-the-web-works.php", 1, "How The Web Works", 1);
 
 INSERT IGNORE INTO tutorials (href, number, name, module_id) 
 VALUES ("/tutorials/the-web/domains.php", 2, "Domains", 1);
+
+INSERT IGNORE INTO tutorials (href, number, name, module_id)
+VALUES ("/tutorials/the-web/common-terms-on-the-web.php", 3, "Common Terms On The Web", 1);
