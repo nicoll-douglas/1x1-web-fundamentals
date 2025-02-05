cf811bbf5924970ba7473bfa417b5d155e8590c8
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-02-05 11:53:20 +0000

    file: delete database schema files

--- config/db/tutorial_modules.sql
+++
@@ -1,6 +0,0 @@
--- Inserts the current tutorial modules into the tutorial_modules table.
-
-USE jiggys_web_fundamentals;
-
-INSERT IGNORE INTO tutorial_modules (number, name) 
-VALUES (1, "The Web");
