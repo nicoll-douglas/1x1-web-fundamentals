346f8aa82f173e462e53967e861e0d417aae0f11
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-02-10 18:38:33 +0000

    update script

--- scripts/php/migration/start.php
+++ scripts/php/migration/start.php
@@ -8,7 +8,7 @@ require_once __DIR__ . "/../../helpers/getMigrationClassName.php";
 require_once __DIR__ . "/../../../config/env.php";
 
 if (!isset($pdo)) {
-  $pdo = connectDb(false);
+  $pdo = connectDb();
 } else {
   $dbName = getenv("DB_NAME");
   $pdo->exec("CREATE DATABASE IF NOT EXISTS $dbName");
